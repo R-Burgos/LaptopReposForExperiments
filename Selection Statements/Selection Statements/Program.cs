02369cfe57fee6021b4f2caf711ad53fa4c05048
Ruben Burgos <code.rburgos@gmail.com>
2022-10-18 17:53:03 -0500

    Updating laptop exercises_work

---
+++ Selection Statements/Selection Statements/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Selection_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //If statements
+            bool condition = true;
+            if (condition)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            int favNumber = 200;
+
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess == favNumber)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else if (userGuess > favNumber)
+            {
+                Console.WriteLine("Too High!");
+            }
+            else
+            {
+                Console.WriteLine("Too Low!");
+            }
+
+            // You can do if statements on their own. You don't have to do else statments
+
+            do 
+            {
+                
+            } while
+        }
+    }
+}
---
+++ W3 Booleans_IfElse/W3 Booleans_IfElse/Program.cs
@@ -0,0 +1,22 @@
+﻿// booleans -> yes/no on/off true/false
+//boolean expression is an expression that returns a bollean value: true or false
+//comparison operators can be used to generate a boolean expression
+
+int x = 10;
+int y = 9;
+Console.WriteLine(x > y); //returns true, b/c 10 is higher than 9
+//or easier:
+Console.WriteLine(10 > 9); // returns true
+Console.WriteLine(x == 10); //returns true
+Console.WriteLine(10 == 15); //returns false
+
+//the boolean value of an expression is the basis for all c# comparisons and conditions
+
+static void SayHello(string firstName, string lastName)
+{
+    Console.WriteLine($"Hello, {firstName} {lastName}. Nice to meet you.");
+}
+
+SayHello("Jeremy", "Huddleston");
+
+
