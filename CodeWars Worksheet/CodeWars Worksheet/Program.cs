8098cab1be276e88eedd8f54fc7d66c7a034d267
Ruben Burgos <code.rburgos@gmail.com>
2022-10-20 16:17:34 -0500

    Updates from 10/20/22

--- CodeWars Worksheet/CodeWars Worksheet/Program.cs
+++ CodeWars Worksheet/CodeWars Worksheet/Program.cs
@@ -2,43 +2,21 @@
 {
     internal class Program
     {
-
-        public static string GetDrinkByProfession(string p)
+        public static double FindAverage(double[] array)
         {
-            switch (p)
+            double sum = 0;
+            for (var i = 0; i < array.Length; i++)
             {
-                case "jabroni":
-                    string d1 = "Patron Tequila";
-                    return d1;
-                case "school counselor":
-                    string d2 = "Anything with Alcohol";
-                    return d2;
-                case "programmer":
-                    string d3 = "Hipster Craft Beer";
-                    return d3;
-                case "bike gang member":
-                    string d4 = "Moonshine";
-                    return d4;
-                case "politician":
-                    string d5 = "Your tax dollars";
-                    return d5;
-                case "rapper":
-                    string d6 = "Cristal";
-                    return d6;
-                default:
-                    string d7 = "Beer";
-                    return d7;
-
+                sum += array[i];
+                return sum;
             }
-            
+            var average = sum / (array.Length);
+
+            return average;
         }
-
-
         static void Main(string[] args)
         {
-            string profession = Console.ReadLine().ToLower();
-            Console.WriteLine (GetDrinkByProfession(profession));
-            
-        }      
+           
+        }
     }
 }
---
+++ Syntax Sugar/Syntax Sugar/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Syntax_Sugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SYNTAX AND SYNTAX SUGAR
+            //Code must be readable in order to be understood by others.
+            //CCR- CLEAR CONCISE AND READABLE
+            //C# developed by Microsoft within .NET framework. Object-oriented based.
+
+            //Syntax is grammar for programming
+            //Clear: code should have a clear purpose and meaning
+            //Concise: should not be unnecessarily long or verbose
+            //Readable: easy to read and understand
+
+            //Best Practices: best thing to do, decided by the coding community
+
+            //Syntax Sugar: not required or necessarily best practice, but makes coding more convenient or conise.
+
+            //Code Smell: any indicator in a program that a deep-seated issue is present in.
+
+            //Type inference vs explicit typing. C# is Strongly(value types cannot be changed) and Statically(every variable must have a type) typed language.
+
+            //Explicit Typing
+            string firstName = "John"; //firstName has a type b/c it was intentionally given a string type by author
+
+            //Inferred Typing
+            var lastName = "Smith"; //when the compiler runs it will assign a type depending on the lastName's value.
+
+            //inline if/Ternary Operator
+            // (conditional) ? true-scope : false-scope;
+            //Option 1
+            var i = 1;
+            string positiveMessage;
+            if (i > 0)
+            { 
+                positiveMessage = "Your are positive"; 
+            }
+            else 
+            {
+                positiveMessage = "You are not positive!"; 
+            }
+            //OR.  Below is syntax sugar
+            var meesage = (i > 0) ? "You are positive!" : "You are not positive!";
+
+
+        }
+    }
+}
