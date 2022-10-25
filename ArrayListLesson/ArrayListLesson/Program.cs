f16d7900d7d940ddb187009ef92ba0100f894d3e
Ruben Burgos <code.rburgos@gmail.com>
2022-10-25 17:00:50 -0500

    Oct25 updates

---
+++ ArrayListLesson/ArrayListLesson/Program.cs
@@ -0,0 +1,51 @@
+﻿// using System;
+// using System.Collections.Generic; not needed in 6.0
+
+using System.Globalization;
+
+namespace ArrayListLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array and List must have the same data types in them.
+            // Arrays are more for a fixed number of objects.
+            // Lists are more flexible, they can grow or shrink as needed.
+
+            //A collection is a class.  You must declare an instance of the class before you can add elements to that collection.
+            // using System.Collection.Generic;
+
+            // Making a new list.
+            // Explicit typing below.
+            List<int> intList = new List<int>() { 8, 7, 4, 58, 131, 847 };
+
+            // Inferred typing below.
+            var myName = new List<string>() { "Ruben", "Javier", "Burgos" };
+            // Add to list.
+            myName.Add("Rodriguez");
+
+            foreach (var name in myName)
+            {
+                Console.WriteLine(name);
+            }
+            
+            // You can use a for loop to go through a List.
+            //Add only even numbers to a list.
+            var numbers = new List<int>();
+            for (int i = 2; i < 100; i += 2)
+            {
+                numbers.Add(i);
+            }
+
+
+            for (var i = 0; i < myName.Count; i++)
+            {
+                Console.WriteLine(myName[i]);
+            }
+
+            //Properties of Lists.  .Count lets you know the total number of objects in a list.  .Add lets you add an object to the list.  .Capacity tells you how many items that list can current hold.
+            
+        }
+    }
+}
--- Testing CodeWars/Testing CodeWars/Program.cs
+++ Testing CodeWars/Testing CodeWars/Program.cs
@@ -1,42 +1,64 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 
 namespace Testing_CodeWars
 {
     internal class Program
     {
-        public static string GetDrinkByProfession(string p)
+        public static int Add(params int[] numbers)
         {
-
-            switch (p)
+            var sum = 0;
+            foreach (var num in numbers)
             {
-                case "jabroni":
-                    Console.WriteLine("Patron Tequila");
-                    break;
-                case "school counselor":
-                    Console.WriteLine("Anything with Alcohol");
-                    break;
-                case "programmer":
-                    Console.WriteLine("Hipster Craft Beer");
-                    break;
-                case "bike gang member":
-                    Console.WriteLine("Moonshine");
-                    break;
-                case "politican":
-                    Console.WriteLine("Your tax dollars");
-                    break;
-                case "rapper":
-                    Console.WriteLine("Cristal");
-                    break;
-                default:
-                    return "Beer";
-                    break;
+                sum += num;
             }
-            
+            return sum;
         }
+
+        public static decimal Add(params decimal[] numbers)
+        {
+            var sum = 0m;
+            foreach (var num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+
         static void Main(string[] args)
         {
-            var profession = Console.ReadLine().ToLower();
-            GetDrinkByProfession(profession);
+
+            Add(1 , 2, 3, 4, 5);
+
+
+
+
+            int[] arr1 = { 4, 3, 2, 5 }; // becomes 4 3 2 6
+            int[] arr2 = { 1, 2, 3, 9 }; // becomes 1 2 4 0
+            int[] arr3 = { 9, 9, 9, 9 }; // becomes 1 0 0 0 0  
+            int[] arr4 = { 0, 1, 3, 8 }; // becomes 0 1 3 8
+
+            /* Given an array of integers of any length, return an array that 
+               has 1 added to the value represented by the array.
+
+            *** the array can't be empty
+
+            *** only non-negative, single digit integers are allowed
+
+               Return nil(or your language's equivalent) for invalid inputs. */
+
+            
+            int position1 = arr1.Length - 1;
+            Console.WriteLine(position1);
+
+
+           
+
+
+
+
+
+
         }
     }
 }
