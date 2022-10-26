96604b3b8254fdddd0e46b38e07736f580de60ee
Ruben Burgos <code.rburgos@gmail.com>
2022-10-25 23:04:05 -0500

    Codewars stuff

--- CodeWars Worksheet/CodeWars Worksheet/Program.cs
+++ CodeWars Worksheet/CodeWars Worksheet/Program.cs
@@ -2,21 +2,55 @@
 {
     internal class Program
     {
-        public static double FindAverage(double[] array)
-        {
-            double sum = 0;
-            for (var i = 0; i < array.Length; i++)
-            {
-                sum += array[i];
-                return sum;
-            }
-            var average = sum / (array.Length);
 
-            return average;
+        //Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e.no negative integers.
+
+
+
+        public static void CountSheep(int n)
+        {
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"{i+1} sheep...");
+            //}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
+
+
+
+
+
         static void Main(string[] args)
         {
-           
+
+            int a = 0;
+            int b = 1;
+            int c = 2;
+            int d = 3;
+            int x = 5;
+
+
+            
+            CountSheep(x);
+            
+
+
+
         }
     }
 }
