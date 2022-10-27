a144735b14292cfd23d352c21e4420929d18b867
Ruben Burgos <code.rburgos@gmail.com>
2022-10-27 17:18:21 -0500

    Class notes

---
+++ Classes/Classes/Drums.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Drums // make keyword public from internal so they interact easier.
+    {
+        //use ctor + tab + tab to auto make a basic constructor
+
+        public Drums() // Defailt Constructor <----------- special method for creating objects
+        {
+
+        }
+
+
+        public Drums(string name)
+        {
+            Brand = name;
+        }
+
+        public Drums(string name, string color, string sound)
+        {
+            Brand = name;
+            Color = color;
+            Sound = sound;  
+        }
+
+        public int numberOfDrums = 5; // Field <------- made default value of 5
+        // if field is private use "_name" best practice
+        // field us camelCase
+
+
+        public string Brand { get; set; } // Property <-----------
+        // get is the read function. set is the write function
+        //properties use PascalCase
+
+        public string Color { get; set; }
+        public string Sound { get; set; } = "Boom!!!"; // assign default value for Sound property
+
+        public string Type { get; set; }
+
+
+        public void HitDrum() // Method <---------
+        {
+            Console.WriteLine($"{Sound}");
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var myDrumKit = new Drums()  // Object Initializer Syntax used here
+            {
+                Type = "Acoustic", 
+                Brand = "Sonor", 
+                Color = "Sparkle White"
+            };
+
+            var my2ndDrumKit = new Drums();
+            my2ndDrumKit.Brand = "Pearl";   // Dot Notation
+            my2ndDrumKit.Color = "Sparkle Red";
+            my2ndDrumKit.Sound = "Crack!!!";
+            my2ndDrumKit.Type = "Acoustic";
+
+            Console.WriteLine(my2ndDrumKit.Brand);
+            Console.WriteLine(my2ndDrumKit.Color);
+            Console.WriteLine(my2ndDrumKit.Sound);
+            Console.WriteLine(my2ndDrumKit.Type);
+
+
+            myDrumKit.HitDrum();
+
+            //Custom Constructor
+            var myOldKit = new Drums("Tama", "Orange", "Loud");
+
+            Console.WriteLine($"My old kit was a {myOldKit.Brand} and it was {myOldKit.Color} and it was  {myOldKit.Sound}!");
+           
+
+        }
+    }
+}
